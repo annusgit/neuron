e8217ebb60ac900e48b44e3e1e299942621a7507
Annus Zulfiqar <azulfiqar.bee15seecs@seecs.edu.pk>
2017-07-27 12:07:02 +0500

    thre layered model

--- Folders/myneuron/myneuron.py
+++ Folders/myneuron/myneuron.py
@@ -35,35 +35,47 @@ pypl.show()
 
 m, n = X.shape
 ins = 2
-hid1_units = 20
+hid1_units = 40
+hid2_units = 20
 outs = 2
 epsilon = 1e-3
 
 Theta1 = epsilon * np.random.randn(ins,hid1_units)
 b1 = np.zeros((1,hid1_units))
 
-Theta2 = epsilon * np.random.randn(hid1_units,outs)
-b2 = np.zeros((1,outs))
+Theta2 = epsilon * np.random.randn(hid1_units,hid2_units)
+b2 = np.zeros((1,hid2_units))
+
+Theta3 = epsilon * np.random.randn(hid2_units,outs)
+b3 = np.zeros((1,outs))
 
 epochs = 40000
 batch_size = 100
 alpha = 1e-4
 losses = []
-
-for e in range(epochs):
-
+e = 1
+#for e in range(epochs):
+while True:
     A2 = np.maximum(0, X.dot(Theta1) + b1)
-    A3 = A2.dot(Theta2) + b2
+    A3 = np.maximum(0, A2.dot(Theta2) + b2)
+    A4 = A3.dot(Theta3) + b3
 
-    exp_A3 = np.exp(A3)
-    prob = exp_A3 / np.sum(exp_A3, axis=1, keepdims=True)
+    exp_scores = np.exp(A4)
+    prob = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
     loss = -1 / m * np.sum(np.log(prob[range(m), y]))
     print('epoch#', e, ' loss = ', loss)
+    e += 1
     losses.append(loss)
 
-    dL_dA3 = prob
-    dL_dA3[range(m), y] -= 1
-    dL_dA3 /= m
+    dL_dA4 = prob
+    dL_dA4[range(m), y] -= 1
+    dL_dA4 /= m
+
+    dL_db3 = dL_dA4.sum(axis=0, keepdims=True)
+    dL_dTh3 = np.dot(A3.T, dL_dA4)
+
+    dL_dA3 = np.dot(dL_dA4, Theta3.T)
+    dL_dA3[A3 <= 0] = 0
 
     dL_db2 = dL_dA3.sum(axis=0, keepdims=True)
     dL_dTh2 = np.dot(A2.T, dL_dA3)
@@ -75,12 +87,16 @@ for e in range(epochs):
     dL_dTh1 = np.dot(X.T, dL_dA2)
 
     Theta1 += -alpha * dL_dTh1
-    b2 += -alpha * dL_db2
-    Theta2 += -alpha * dL_dTh2
     b1 += -alpha * dL_db1
 
+    Theta2 += -alpha * dL_dTh2
+    b2 += -alpha * dL_db2
 
-train_acc = 100 * np.mean(np.argmax(A3, axis=1) == y)
+    Theta3 += -alpha * dL_dTh3
+    b3 += -alpha * dL_db3
+
+
+train_acc = 100 * np.mean(np.argmax(A4, axis=1) == y)
 print('Training accuracy = ', train_acc)
 
 pypl.figure(2)
